bb95e33c88fb9a0ae61be6d0a46248ef8bbd6eb3
Jakaria Blaine <jakariablaine120@gmail.com>
2021-03-18 01:15:35 +0600

    Add shadow

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ import { OrbitControls } from './lib/OrbitControls.js'
 const scene = new THREE.Scene();
 
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 15000);
+const camera = new THREE.PerspectiveCamera(85, window.innerWidth / window.innerHeight, .1, 1500);
 
 camera.position.y = 2;
+camera.position.x = 2;
 camera.position.z = 3;
 
 camera.lookAt(0, 0, 0);
@@ -14,6 +15,10 @@ camera.lookAt(0, 0, 0);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = true;
+
+
 document.getElementById('playground').appendChild(renderer.domElement);
 
 const orbitControl = new OrbitControls(camera, renderer.domElement)
@@ -25,7 +30,7 @@ const player_material = new THREE.MeshPhongMaterial({ color: 0xff0000});
 const player_object = new THREE.Mesh(player_geometry, player_material)
 
 player_object.position.y = 0.5;
-console.log(player_object.position)
+player_object.castShadow = true;
 
 scene.add(player_object)
 
@@ -34,19 +39,24 @@ const intensity = 1;
 
 const light = new THREE.DirectionalLight(color, intensity);
 
-light.position.set(5, 10, 0);
+light.position.set(150, 100, 5);
 light.target.position.set(0, 0, 0);
+light.shadow.mapSize.width = 1000
+light.shadow.mapSize.height = 1000
+light.castShadow = true;
+
 scene.add(light);
 scene.add(light.target);
 
 
-const groundGeo = new THREE.PlaneGeometry( 10, 10 );
-const groundMat = new THREE.MeshPhongMaterial( { color: 0xf1f1f1, side: THREE.DoubleSide } );
+const groundGeo = new THREE.PlaneGeometry( 10, 10, 5, 5);
+const groundMat = new THREE.MeshPhongMaterial( { color: 0xf1f1f1} );
 
 const ground = new THREE.Mesh( groundGeo, groundMat );
 
 ground.position.y = 0;
 ground.rotation.x = - Math.PI / 2;
+ground.castShadow = false;
 ground.receiveShadow = true;
 scene.add( ground );
 
